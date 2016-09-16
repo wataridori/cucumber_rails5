8c9c26e5ffdfb0a2ab3cf6cc5ec21c1edccd431d
Thien To Duc <to.duc.thien@framgia.com>
2016-09-16 10:01:12 +0700

    correct deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,36 +15,10 @@ set :passenger_environment_variables, {}
 set :passenger_restart_command, "passenger-config restart-app"
 set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# NOTE: public/uploads IS USED ONLY FOR THE STAGING ENVIRONMENT
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 set :default_env, {
   database_db_name: ENV["DATABASE_DB_NAME"],
   database_hostname: ENV["DATABASE_HOSTNAME"],
